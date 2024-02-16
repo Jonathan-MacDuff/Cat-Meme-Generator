3ac2063ef46d32dcb77de59ab2816a460a40e021
Jonathan MacDuff <jonathan.macduff@outlook.com>
2024-02-15 17:39:55 -0800

    Added basic cat image generation functionality for the Mean Cat button

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 function getCat() {
-    fetch("http://placekitten.com/200/300")
+    return fetch("https://api.thecatapi.com/v1/images/search?limit=10")
         .then((resp) => resp.json())
         .then((json) => {
-            return json;
+            return json[0].url;
         })
         .catch((error) => {
             console.error("Error fetching cat:", error);
             throw error;
         });
 };
+
+const niceCatButton = document.getElementById("nice-cat-button");
+const meanCatButton = document.getElementById("mean-cat-button");
+const badassCatButton = document.getElementById("badass-cat-button");
+
+const catContainer = document.getElementById("cat-container");
+
+meanCatButton.addEventListener("click", function() {
+    getCat()
+        .then((catData) => {
+            console.log("Cat data:", catData);
+            const meanCatImg = document.createElement("img");
+            meanCatImg.src = catData;
+            meanCatImg.alt = "Mean Cat";
+
+            catContainer.appendChild(meanCatImg);
+        })
+        .catch((error) => console.error("Error:", error));
+});
